e77dce71864df70f519d17c3754ecf0ee5f18823
Howard Su <howard0su@gmail.com>
2024-05-06 18:46:15 -0700

    remove dead code

--- zynq/peri.h
+++ zynq/peri.h
@@ -6,9 +6,5 @@
 void peri_init();
 void peri_free();
 
-#define FPGA_ID_RX14_WF4 4
-
-#define GPIO_READ_BIT(x) (fpga_status->rx_fifo >= 512)
-
 void rf_enable_airband(bool enabled);
 void rf_attn_set(float attn_dB);

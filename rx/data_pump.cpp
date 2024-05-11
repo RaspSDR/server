c24d5d1fa2b76b22c28d6794ec1b1794868d256b
Howard Su <howard0su@gmail.com>
2024-05-10 17:30:52 -0700

    rx fifo counter is per sample

--- rx/data_pump.cpp
+++ rx/data_pump.cpp
@@ -71,9 +71,6 @@ static void snd_service()
             i_samps[ch] = rx->in_samps[rx->wr_pos];
         }
 
-        int avail_data = fpga_status->rx_fifo;
-        if (avail_data < nrx_samps * 2)
-            printf("RX Underflow %d\n", avail_data);
         for (int i = 0; i < nrx_samps;i++)
         {
             s4_t data[MAX_RX_CHANS * 2];
@@ -213,7 +210,7 @@ static void data_pump(void *param)
             }
         #else
             //TaskSleepReason("wait for interrupt");
-            while (fpga_status->rx_fifo < nrx_samps * 2 + 10) {
+            while (fpga_status->rx_fifo < nrx_samps * 2 * rx_chans) {
                 TaskSleepMsec(1);
             }
         #endif

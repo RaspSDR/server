ee755aebdc4ec64acd109630760ef5ca1d8a6684
Howard Su <howard0su@gmail.com>
2024-05-22 05:08:18 -0700

    modify for latest fpga

--- app/write-c2-files.c
+++ app/write-c2-files.c
@@ -17,42 +17,40 @@
 #include <unistd.h>
 
 #define RESET_RX (1 << 0)
-#define RESET_WF0 (1 << 1)
-#define RESET_WF1 (1 << 2)
-#define RESET_WF2 (1 << 3)
-#define RESET_WF3 (1 << 4)
+#define RESET_WF(n) (1 << (n + 1))
 
-typedef struct
-{
-  uint16_t reset;
-  uint16_t siggen;
-  uint32_t rx_freq[8];
-  struct
-  {
-    uint32_t wf_freq;
-    uint32_t wf_decim;
-  } wf_config[4];
-  uint32_t siggen_freq;
+#define RESET_LEDON (1 << 7)
+#define RESET_PPS (1 << 8)
+
+#define GPIO_ANTENNA(n) (1 << (n + 0))
+
+#define GPIO_DITHER (1 << 6)
+#define GPIO_PGA (1 << 7)
+
+typedef struct {
+    uint32_t reset;
+    uint32_t rx_freq[16];
+    struct {
+        uint32_t wf_freq;
+        uint32_t wf_decim;
+    }wf_config[4];
+    uint8_t gpios;
 }__attribute__((packed)) FPGA_Config;
 
-typedef struct
-{
-  uint32_t rx_fifo;
-  uint32_t wf_fifo[4];
-  uint32_t pps_fifo;
-  uint64_t fpga_dna;
+typedef struct {
+    uint32_t signature;
+    uint32_t rx_fifo;
+    uint32_t wf_fifo[4];
+    uint32_t pps_fifo;
+    uint64_t fpga_dna;
+    uint64_t tsc;
 }__attribute__((packed)) FPGA_Status;
 
-typedef struct
-{
-  uint32_t rx_data;
-  uint32_t wf_data[4];
-}__attribute__((packed)) FPGA_Data;
-
-volatile FPGA_Config *fpga_config;
-volatile const FPGA_Status *fpga_status;
-volatile FPGA_Data *fpga_data;
+const volatile int32_t *fpga_rx_data;
+const volatile uint32_t *fpga_wf_data[4];
 const volatile uint32_t *fpga_pps_data;
+volatile FPGA_Config *fpga_config;
+const volatile FPGA_Status *fpga_status;
 
 void print_current_time_with_ms(void)
 {
@@ -91,23 +89,29 @@ int main(int argc, char *argv[])
   int chan[8] = {1, 1, 1, 1, 1, 1, 1, 1};
   uint8_t value = 0;
 
-  if ((fd = open("/dev/mem", O_RDWR)) < 0)
-  {
+    if ((fd = open("/dev/mem", O_RDWR)) < 0)
+    {
     fprintf(stderr, "Cannot open /dev/mem.\n");
     return EXIT_FAILURE;
-  }
+    }
 
-  fpga_config = mmap(NULL, sysconf(_SC_PAGESIZE), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0x40000000);
-  fpga_status = mmap(NULL, sysconf(_SC_PAGESIZE), PROT_READ, MAP_SHARED, fd, 0x41000000);
-  fpga_data = mmap(NULL, 32 * sysconf(_SC_PAGESIZE), PROT_READ, MAP_SHARED, fd, 0x42000000);
+    fpga_config = (FPGA_Config *)mmap(NULL, sysconf(_SC_PAGESIZE), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0x40000000);
+    fpga_status = (FPGA_Status *)mmap(NULL, sysconf(_SC_PAGESIZE), PROT_READ, MAP_SHARED, fd, 0x41000000);
+    fpga_rx_data = (const int32_t *)mmap(NULL, 2 * sysconf(_SC_PAGESIZE), PROT_READ, MAP_SHARED, fd, 0x42000000);
 
+    for (int i = 0; i < 4; i++)
+    {
+        fpga_wf_data[i] = (const uint32_t *)mmap(NULL, 1 * sysconf(_SC_PAGESIZE), PROT_READ, MAP_SHARED, fd, 0x43000000 + 0x01000000 * i);
+    }
 
     fpga_pps_data =  (const uint32_t *)mmap(NULL, 1 * sysconf(_SC_PAGESIZE), PROT_READ, MAP_SHARED, fd, 0x47000000);
 
+    close(fd);
 
   fpga_config->reset = 0;
-  fpga_config->reset = 0xffff;
 
+
+  fpga_config->reset |= 0xff;
   for (i = 0; i < 8; ++i)
   {
     fpga_config->rx_freq[i] = MAKE_FREQ(freq[i], corr);
@@ -119,26 +123,10 @@ int main(int argc, char *argv[])
     fpga_config->wf_config[i].wf_decim = 1 << (i + 3);
   }
 
+  int wf_channels = (fpga_status->signature >> 8) & 0x0f;
+  int rx_channels = (fpga_status->signature) & 0x0f;
+
   printf("DNA: %llx\n", fpga_status->fpga_dna);
-  printf("Count RX Addr&: %x\n", &fpga_status->rx_fifo);
-  printf("Count WF0 Addr&: %x\n", &fpga_status->wf_fifo[0]);
-  printf("Count WF1 Addr&: %x\n", &fpga_status->wf_fifo[1]);
-  printf("Count WF2 Addr&: %x\n", &fpga_status->wf_fifo[2]);
-  printf("Count WF3 Addr&: %x\n", &fpga_status->wf_fifo[3]);
-
-  printf("RX\tWF0\tWF1\tWF2\tWF3\tPPS\n");
-  for (int i = 0; i < 1000; i++)
-  {
-    printf("%d\t%d\t%d\t%d\t%d\t%d\n", fpga_status->rx_fifo, fpga_status->wf_fifo[0], fpga_status->wf_fifo[1], fpga_status->wf_fifo[2], fpga_status->wf_fifo[3], fpga_status->pps_fifo);
-    if (fpga_status->pps_fifo > 0)
-    {
-      printf("PPS=%lu\n", *fpga_pps_data);
-    }
-    sleep(1);
-  }
-
-  fpga_config->reset &= ~RESET_RX;
-  fpga_config->reset |= RESET_RX;
 
   offset = 0;
 
@@ -146,9 +134,11 @@ int main(int argc, char *argv[])
 
   clock_gettime(CLOCK_REALTIME, &start_spec);
 
-
   printf("DNA: %llx\n", fpga_status->fpga_dna);
-  printf("Count Addr&: %x\n", &fpga_status->rx_fifo);
+//  printf("Count Addr&: %x\n", fpga_status->rx_fifo);
+  printf("Signature: 0x%x\n", fpga_status->signature);
+
+  int32_t tick, last_tick = 0;
 
   while (offset < 24000)
   {
@@ -158,23 +148,30 @@ int main(int argc, char *argv[])
       usleep(1000);
     }
       
-
-    for (i = 0; i < 250; ++i)
+    for (i = 0; i < 200; ++i)
     {
-      for (j = 0; j < 8; ++j)
+      tick = *fpga_rx_data;
+      if (last_tick == 0)
+        printf("get tick: %d\n", tick);
+      else
+        printf("tick diff: %d\n", tick - last_tick);
+      last_tick = tick;
+      for (j = 0; j < rx_channels; ++j)
       {
-        int data0 = fpga_data->rx_data;
-        int data1 = fpga_data->rx_data;        
+        int data0 = *fpga_rx_data;
+        int data1 = *fpga_rx_data;        
       }
     }
 
-    offset += 250;
+    offset += 200;
     // printf("%d\n", offset);
   }
 
   clock_gettime(CLOCK_REALTIME, &stop_spec);
 
   printf("RX: sample rate= %f (should close to 12,000, otherwise there is bug)\n", 24000.0 / (stop_spec.tv_sec - start_spec.tv_sec + (stop_spec.tv_nsec - start_spec.tv_nsec) * 1e-9));
+
+  #if 0
   printf("Count Addr&: %x\n", &fpga_status->wf_fifo[0]);
 
   for (int i = 0; i < 1; i++)
@@ -205,6 +202,6 @@ int main(int argc, char *argv[])
     clock_gettime(CLOCK_REALTIME, &stop_spec);
     printf("RX: sample rate= %f (should close to 12000, otherwise there is bug)\n", 24000.0 / (stop_spec.tv_sec - start_spec.tv_sec + (stop_spec.tv_nsec - start_spec.tv_nsec) * 1e-9));
   }
-
+#endif
   return EXIT_SUCCESS;
 }

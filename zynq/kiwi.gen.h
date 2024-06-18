f730e8c958fe61b37b99cc02f4a85920ad86db95
Howard Su <howard0su@gmail.com>
2024-06-17 17:57:29 -0700

    remove unused constant

--- zynq/kiwi.gen.h
+++ zynq/kiwi.gen.h
@@ -11,7 +11,7 @@
 #define SND_RATE_3CH 20250    // DEFp 0x4f1a
 #define MIN_SND_RATE 12000    // DEFp 0x2ee0
 #define SND_RATE_4CH 12000    // DEFp 0x2ee0
-#define NRX_SPI 2047    // DEFp 0x7ff
+
 #define RXO_BITS 31    // DEFp 0x18
 #define GPS_MAX_CHANS 32    // DEFp 0xc
 

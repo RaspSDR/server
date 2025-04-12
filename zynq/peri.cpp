51467cbb83320f126d2f306406b0e48b394bdb18
Howard Su <howard0su@gmail.com>
2025-04-12 10:39:29 +0800

    respect snd_rate and rx_decim variable

--- zynq/peri.cpp
+++ zynq/peri.cpp
@@ -202,7 +202,7 @@ u64_t fpga_dna() {
 }
 
 void fpga_start_rx() {
-    uint32_t decim = uint32_t(ADC_CLOCK_NOM / 12000 / 256);
+    uint32_t decim = uint32_t(rx_decim / 256);
     int rc = ioctl(ad8370_fd, RX_START, decim);
     if (rc)
         sys_panic("Start RX failed");
